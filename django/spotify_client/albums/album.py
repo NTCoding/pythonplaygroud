99842ac6a7314289a81180f7e37549522da616cb
Nick <nick.tune@artirix.com>
2014-06-23 13:06:39 +0100

    converted album search to use custom format by modifying spotify json

--- django/spotify_client/albums/album.py
+++ django/spotify_client/albums/album.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 import requests
+import json
 
 def search(request):
     query = request.GET.get("query")
     spotify_url = "http://ws.spotify.com/search/1/album.json?q=" + query
-    response = requests.get(spotify_url)
-    return HttpResponse(response.text, content_type="application/json")
+    response_json = requests.get(spotify_url).json()
+    albums = [ a['name'] for a in response_json['albums'] ]
+    response = json.dumps({ "albums": albums })
+    return HttpResponse(response, content_type="application/json")
