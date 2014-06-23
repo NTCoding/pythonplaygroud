97e7de8146d1918f5eafd4fbf20deb6c28afa515
Nick <nick.tune@artirix.com>
2014-06-23 15:48:14 +0100

    updated albums endpoint to return image and url

--- django/spotify_client/albums/album.py
+++ django/spotify_client/albums/album.py
@@ -4,8 +4,17 @@ import json
 
 def search(request):
     query = request.GET.get("query")
-    spotify_url = "http://ws.spotify.com/search/1/album.json?q=" + query
+    spotify_url = "https://api.spotify.com/v1/search?q=" + query + "&type=album"
     response_json = requests.get(spotify_url).json()
-    albums = [ a['name'] for a in response_json['albums'] ]
+    albums = [  __toAlbum(a) for a in response_json['albums']['items'] ]
     response = json.dumps({ "albums": albums })
     return HttpResponse(response, content_type="application/json")
+
+def __toAlbum(album):
+	return {
+		"album" : {
+		    "name": album['name'],
+            "image": album['images'][0],
+            "link": album['uri']
+		}
+	}
