00113da0f08e919a4e0fbc2a342eafb313f56d9b
Nick <nick.tune@artirix.com>
2014-06-23 12:40:57 +0100

    updated album search to make request to spotify and immediately render the returned json

--- django/spotify_client/albums/album.py
+++ django/spotify_client/albums/album.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-
+import requests
 
 def search(request):
     query = request.GET.get("query")
-    return HttpResponse("Album search for: %s" % query)
+    spotify_url = "http://ws.spotify.com/search/1/album.json?q=" + query
+    response = requests.get(spotify_url)
+    return HttpResponse(response.text, content_type="application/json")
