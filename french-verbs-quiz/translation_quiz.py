11380fb0a84c6bd2940ac603922e350454f4817d
nick <nick.tune@digital.voa.gov.uk>
2016-05-10 15:01:30 +0100

    improve output of quiz

--- french-verbs-quiz/translation_quiz.py
+++ french-verbs-quiz/translation_quiz.py
@@ -4,32 +4,44 @@ import time
 def main():
     verbs_file = "translations.tsv"
     verbs = parse_verbs(verbs_file)
-    while True:
+    incorrect_answers = []
+    for x in range(0, 20):
         verb = random.choice(verbs) 
-        quiz_user(verb)
+        failure = quiz_user(verb)
+        if (failure):
+            incorrect_answers.append(failure)
+        time.sleep(1)    
+        print("")
         print("Question suivante....")
-        print("\n\n")
+        print("")
         time.sleep(3)
+    print_failures(incorrect_answers)
 
 def quiz_user(verb):
     print("Quel est le sens de " + verb[0] + "?")
     answer = input()
-    print("Tu as dit: " + answer + ".")
-    time.sleep(2)
-    print("La bonne réponse est " + verb[1])
+    print("Tu as dit: '" + answer + "'")
+    time.sleep(1)
+    print("La bonne réponse est '" + verb[1] + "'")
     time.sleep(1)
     if answer.strip() == verb[1].strip():
         print("Vous avez raison!")
     else:
-        print("Désolé. Votre response est incorrecte. Vous devez pratiquer " + verb[0])
-    time.sleep(2)
-    print("")
+        print("Désolé. Votre response est incorrecte.")
+        return (verb[0], answer)
 
 def parse_verbs(file):
     verbs = []
     for line in open(file):
         parts = line.split("\t")
-        verbs.append((parts[0], parts[1]))
+        verbs.append((parts[0].strip(), parts[1].strip()))
     return verbs
+
+def print_failures(failures):
+    print()
+    print("You answered incorrectly to...")
+    print()
+    for f in failures:
+        print(f[0] + " - you said: " + f[1])
 
 main()
